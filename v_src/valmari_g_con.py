6623f17ee68682473be1b294a91fcb765da75b05
Filippo Biondi <filippo.biondi@imtlucca.it>
2025-06-26 21:11:07 +0200

    clean

--- v_src/valmari_g_con.py
+++
@@ -1,4 +0,0 @@
-with open("graph.txt", "r") as f:
-    ns = list(map(lambda x: x-1, map(int," ".join(f.read().split("\n")).split(" ")[5:-1])))
-    for i in range(0, len(ns),3):
-        print(ns[i],ns[i+2])
